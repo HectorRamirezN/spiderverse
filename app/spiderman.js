abaf269d78092f1f5d75660c714267ca258f916a
Hector Ramirez <hector.rn@hotmail.com>
2022-04-26 15:01:30 -0600

    Added unit test for Spiderman class

---
+++ app/spiderman.js
@@ -0,0 +1,10 @@
+class Spiderman {
+  constructor(name, age, actor, movie, studio) {
+    this.name = name;
+    this.age = age;
+    this.actor = actor;
+    this.movies = movie;
+    this.studio = studio;
+  }
+}
+module.exports = Spiderman;
